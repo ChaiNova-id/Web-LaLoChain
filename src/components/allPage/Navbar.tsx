6744e30c0561ffdb8b85e50cb887e57a9afb674c
eben4ya <benaya.imanuela@gmail.com>
2025-04-29 12:38:53 +0700

    refactor: navbar component

--- src/components/allPage/Navbar.tsx
+++ src/components/allPage/Navbar.tsx
@@ -1,29 +1,36 @@
-import Image from "next/image";
+// TODO: add brand text style each navlink when routing different routes
 
-const Navbar = () => {
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+interface NavbarProps {
+  navlink: string[];
+}
+
+const Navbar = ({ navlink }: NavbarProps) => {
   return (
     <div className="w-[1920px] h-20 px-36 py-5 bg-neutral-50 shadow-2 border-b-2 border-brand-100 inline-flex justify-between items-center gap-5 overflow-hidden">
       <div className="flex flex-row justify-center items-center gap-5 cursor-default">
         <Image src="/images/Logo.svg" alt="Logo" width={40} height={40} />
-        <div className="flex-1 justify-start text-black text-2xl font-bold">
+        <div className="flex-1 justify-start text-black heading-6">
           LaLoChain
         </div>
       </div>
       <div className="bg-neutral-50 flex justify-center items-center gap-10">
-        <div className="justify-start text-neutral-950 hover:text-brand-500 text-base font-bold cursor-pointer">
-          Home
-        </div>
-        <div className="justify-start text-neutral-950 hover:text-brand-500 text-base font-bold cursor-pointer">
-          Marketplace
-        </div>
-        <div className="justify-start text-neutral-950 hover:text-brand-500 text-base font-bold cursor-pointer">
-          Owners
-        </div>
-        <div className="w-40 p-3 bg-brand-600 rounded-lg inline-flex flex-col justify-center items-center cursor-pointer">
-          <div className="justify-start text-neutral-50 text-base font-bold">
-            Connect
+        {navlink.map((link, index) => (
+          <div
+            key={index}
+            className="justify-start text-neutral-950 hover:text-brand-500 heading-9 cursor-pointer"
+          >
+            {link}
           </div>
-        </div>
+        ))}
+        <Button
+          variant="primaryBrand"
+          className="w-40 p-6 text-neutral-50 heading-9"
+        >
+          Connect
+        </Button>
       </div>
     </div>
   );
