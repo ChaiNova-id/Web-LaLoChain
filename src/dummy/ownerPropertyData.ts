28339811be5f47e6895516a4b4e25925ef0428ac
eben4ya <benaya.imanuela@gmail.com>
2025-04-29 21:14:16 +0700

    feat: data dummy folder

---
+++ src/dummy/ownerPropertyData.ts
@@ -0,0 +1,59 @@
+import { Property } from "@/types/propertyDashboardTypes";
+
+export const properties: Property[] = [
+  {
+    id: "1",
+    name: "G&B House",
+    description: "A beautiful house in Surakarta",
+    location: "Surakarta",
+    rentalIncome: "$300/month",
+    tokenizedValue: "$3,200",
+    status: "Verified",
+  },
+  {
+    id: "2",
+    name: "Lotus Villa",
+    description: "Cozy villa with garden view",
+    location: "Bandung",
+    rentalIncome: "$400/month",
+    tokenizedValue: "$4,000",
+    status: "Not Verified",
+  },
+
+  {
+    id: "3",
+    name: "Sunset Apartment",
+    description: "Modern apartment with city view",
+    location: "Jakarta",
+    rentalIncome: "$500/month",
+    tokenizedValue: "$5,500",
+    status: "Verified",
+  },
+  {
+    id: "4",
+    name: "Ocean Breeze Cottage",
+    description: "Cozy cottage near the beach",
+    location: "Bali",
+    rentalIncome: "$600/month",
+    tokenizedValue: "$6,000",
+    status: "Not Verified",
+  },
+  {
+    id: "5",
+    name: "Mountain Retreat",
+    description: "Peaceful retreat in the mountains",
+    location: "Malang",
+    rentalIncome: "$450/month",
+    tokenizedValue: "$4,800",
+    status: "Verified",
+  },
+  {
+    id: "6",
+    name: "City Center Loft",
+    description: "Stylish loft in the heart of the city",
+    location: "Yogyakarta",
+    rentalIncome: "$550/month",
+    tokenizedValue: "$5,700",
+    status: "Not Verified",
+  },
+];
