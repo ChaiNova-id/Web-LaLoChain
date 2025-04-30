7c739db89965f74a1f6a506f68721c42725125c9
eben4ya <benaya.imanuela@gmail.com>
2025-05-01 00:53:57 +0700

    feat: key features component (wip)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/landingPage/Hero";
+import KeyFeatures from "@/components/landingPage/KeyFeatures";
 
 export default function Home() {
   return (
-    <>
+    <div className="space-y-16">
       <Hero />
-    </>
+      <KeyFeatures />
+    </div>
   );
 }
