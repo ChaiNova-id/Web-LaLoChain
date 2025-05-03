6f56ea4856d32b5e45d06143196e79f1bab746f8
eben4ya <benaya.imanuela@gmail.com>
2025-05-03 17:52:18 +0700

    fix: hero component

--- src/components/landingPage/Hero.tsx
+++ src/components/landingPage/Hero.tsx
@@ -2,26 +2,25 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="self-stretch w-full py-[60px] relative bg-black/60 inline-flex justify-center items-center gap-[60px] overflow-hidden">
+    <div className="self-stretch w-screen py-[60px] -mt-5 mx-[-144px] relative bg-brand-900 inline-flex justify-center items-center gap-[60px] overflow-hidden">
       <div className="flex-1 inline-flex flex-col justify-start items-center gap-6">
-        <div className="w-[520px] text-center justify-start text-white text-[40px] font-bold font-['Roboto'] leading-[48px]">
+        <div className="w-[520px] text-center justify-start text-neutral-50 text-[40px] font-bold leading-[48px]">
           Invest in Rental Income Tokenization
         </div>
-        <div className="w-[520px] text-center justify-start text-white text-base font-normal font-['Roboto'] leading-normal">
+        <div className="w-[520px] text-center justify-start text-neutral-100 text-base font-normal leading-normal">
           Empowering property owners and investors through blockchain technology
         </div>
         <Link
           href="/marketplace"
           className="flex flex-col justify-start items-start gap-3 overflow-hidden"
         >
-          <div className="w-60 p-3 bg-black rounded-lg flex flex-col justify-center items-center">
-            <div className="justify-start text-white text-base font-medium font-['Roboto'] leading-normal">
+          <div className="w-60 p-3 bg-brand-600 rounded-lg flex flex-col justify-center items-center">
+            <div className="justify-start text-neutral-50 text-base font-medium leading-normal">
               Browse Properties
             </div>
           </div>
         </Link>
       </div>
-      <div className="w-[1440px] h-0 left-0 top-[360px] absolute outline-1 outline-offset-[-0.50px] outline-black/10" />
     </div>
   );
 };
