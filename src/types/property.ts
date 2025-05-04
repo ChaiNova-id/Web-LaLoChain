cec7ef8df069a49fdde42e5670e4f586a13980ba
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-05-04 17:00:32 +0700

    fix/change paginated property response

--- src/types/property.ts
+++ src/types/property.ts
@@ -2,7 +2,8 @@ import { Property } from "@prisma/client";
 import { PaginationMeta } from "./api";
 
 export type PaginatedPropertyResponse = {
-  owner: string;
+  owner?: string;
+  investor?: string;
   data: Property[];
   pagination: PaginationMeta;
 };
